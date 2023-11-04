31a68f024a4f173c7d3937bb29cd88868d8eddb0
Luis Rodriguez <rodrigueznunezluis19@stud.ase.ro>
2023-11-04 14:39:57 +0200

    i made the constructor for the TableColumn , before i was trying to make them in the class createtable this time i have made them in the tableColumn as they are public

--- ProjectTeam/ProjectTeam/FileName.cpp
+++ ProjectTeam/ProjectTeam/FileName.cpp
@@ -8,5 +8,8 @@ public:
 	std::string type;
 	int size;
 	std::string default_value;
+
+	TableColumn(const std::string& name,const std::string& type, int size,const std::string& default_value = "")
+		: name(name), type(type), size(size), default_value(default_value) {}
 };
 
