b5cdd7d35aa819bcccbf3b4313b804804ae42c60
Luis Rodriguez <rodrigueznunezluis19@stud.ase.ro>
2023-11-12 00:33:26 +0200

    creation of the class Table  public and defining

---
+++ ProjectTeam/ProjectTeam/Source.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <String>
+#include <vector>
+
+class TableColumn {
+public:
+	std::string name;
+	std::string type;
+	int size;
+	std::string default_value;
+
+	TableColumn(const std::string& name, const std::string& type, int size, const std::string& default_value = "")
+		: name(name), type(type), size(size), default_value(default_value) {}
+};
+
+class Table {
+public:
+	std::string name;
+	std::vector<TableColumn> columns;
+};
--- ProjectTeam/ProjectTeam/Source1.cpp
+++ ProjectTeam/ProjectTeam/Source1.cpp
@@ -9,7 +9,7 @@ public:
 	int size;
 	std::string default_value;
 
-	TableColumn(const std::string& name,const std::string& type, int size,const std::string& default_value = "")
+	TableColumn(const std::string& name, const std::string& type, int size, const std::string& default_value = "")
 		: name(name), type(type), size(size), default_value(default_value) {}
 };
 
